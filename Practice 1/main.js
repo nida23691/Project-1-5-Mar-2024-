1db03945c917865ddc9bb92bdbc89a8818dd9b98
nida23691 <160360027+nida23691@users.noreply.github.com>
2024-03-06 00:56:50 +0500

    Add files via upload

---
+++ Practice 1/main.js
@@ -0,0 +1,47 @@
+var person_name = "Nida"; // This saves the name
+console.log("Hello ".concat(person_name, ", would you like to learn some CSS today?")); // This shows the message
+// ---------------------------------------------------------------------------------------------------------
+var person_name = "Nida"; // This saves the name
+console.log(person_name.toLowerCase()); // Shows the name in all small letters
+console.log(person_name.toUpperCase()); // Shows the name in ALL BIG LETTERS
+console.log(person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase()); // Shows the name with the First Letter Big
+// ------------------------------------------------------------------------------------------------------------------------------------
+console.log('Newtons once said, "For every action ther is an eaqual and opposite reaction."');
+// ------------------------------------------------------------------------------------------------
+var famous_person = "Newtons";
+var message = "".concat(famous_person, " once said, \"For every action ther is an eaqual and opposite reaction.\"");
+console.log(message);
+// ---------------------------------------------------------------------------------------------------------
+var person_name = "\t\n Nida \t\n"; // This saves the name with whitespace
+console.log(person_name); // Shows the name with whitespace
+console.log(person_name.trim()); // Shows the name without whitespace
+// ---------------------------------------------------------------------------
+console.log(5 + 3); // Addition
+console.log(10 - 2); // Substraction
+console.log(4 * 2); // Multiplication
+console.log(16 / 2); // Division
+// ---------------------------------------------------------------------------
+console.log(5 + 3); // Addition
+console.log(5 + 3); // Addition
+console.log(5 + 3); // Addition
+console.log(5 + 3); // Addition
+// ---------------------------------------------------------------------------
+var favoriteNumber = 4; // This saves your favorite number
+console.log("My favorite number is ".concat(favoriteNumber, ".")); // This shows the message with your favorite number
+// --------------------------------------------------------------------------------------------------------------
+// Nida, 2024-03-05
+// This program prints a personal message
+var myName = "Nida";
+console.log("Hello ".concat(myName, ", would you like to learn some CSS today?"));
+// --------------------------------------------------------------------------------------------------------------
+var names = ["Mehak", "Nazia", "Sobia"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+// -----------------------------------------------------------------------------------------------------------------
+var names = ["Mehak", "Nazia", "Sobia"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hello ".concat(name_1, ", would you like to learn some CSS today?"));
+}
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Practice 1/main.ts
@@ -0,0 +1,49 @@
+let person_name= "Nida" // This saves the name
+console.log(`Hello ${person_name}, would you like to learn some CSS today?`) // This shows the message
+// ---------------------------------------------------------------------------------------------------------
+let person_name= "Nida" // This saves the name
+
+console.log(person_name.toLowerCase()) // Shows the name in all small letters
+console.log(person_name.toUpperCase()) // Shows the name in ALL BIG LETTERS
+console.log(person_name.charAt(0).toUpperCase() + person_name.slice(1).toLowerCase()) // Shows the name with the First Letter Big
+// ------------------------------------------------------------------------------------------------------------------------------------
+console.log('Newtons once said, "For every action ther is an eaqual and opposite reaction."')
+// ------------------------------------------------------------------------------------------------
+let famous_person= "Newtons"
+let message= `${famous_person} once said, "For every action ther is an eaqual and opposite reaction."`
+
+console.log(message)
+// ---------------------------------------------------------------------------------------------------------
+let person_name= "\t\n Nida \t\n" // This saves the name with whitespace
+
+console.log(person_name) // Shows the name with whitespace
+console.log(person_name.trim()) // Shows the name without whitespace
+// ---------------------------------------------------------------------------
+console.log(5 + 3) // Addition
+console.log(10 - 2) // Substraction
+console.log(4 * 2) // Multiplication
+console.log(16 / 2) // Division
+// ---------------------------------------------------------------------------
+console.log(5 + 3) // Addition
+console.log(5 + 3) // Addition
+console.log(5 + 3) // Addition
+console.log(5 + 3) // Addition
+// ---------------------------------------------------------------------------
+let favoriteNumber= 4 // This saves your favorite number
+console.log(`My favorite number is ${favoriteNumber}.`) // This shows the message with your favorite number
+// --------------------------------------------------------------------------------------------------------------
+// Nida, 2024-03-05
+// This program prints a personal message
+let myName= "Nida"
+console.log(`Hello ${myName}, would you like to learn some CSS today?`)
+// --------------------------------------------------------------------------------------------------------------
+let names= ["Mehak", "Nazia", "Sobia"]
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i])
+}
+// -----------------------------------------------------------------------------------------------------------------
+let names= ["Mehak", "Nazia", "Sobia"]
+for (let name of names) {
+    console.log(`Hello ${name}, would you like to learn some CSS today?`)
+}
+// --------------------------------------------------------------------------------------------------------------------
