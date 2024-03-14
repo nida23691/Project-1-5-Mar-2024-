6b223fde85f29a0e0c0a8e14f4e186636b064162
nida23691 <160360027+nida23691@users.noreply.github.com>
2024-03-15 00:50:23 +0500

    Update main.ts

--- Practice 1/main.ts
+++ Practice 1/main.ts
@@ -350,3 +350,34 @@ console.log(city_country("Karachi", "Pakistan"));
 console.log(city_country("China", "Korea"));
 console.log(city_country("London", "Singapore"));
 // ----------------------------------------------------------------------------------------------------------------------
+function make_album(artist: string, title: string, tracks?: number) {
+    let album = { artist, title };
+    if (tracks) {
+        album['tracks'] = tracks;
+    }
+    return album;
+}
+
+console.log(make_album("Artist One", "The First Album"));
+console.log(make_album("Artist Two", "The Second Album"));
+console.log(make_album("Artist Three", "The Third Album", 44));
+// ----------------------------------------------------------------------------------------------------------------------
+let magicians: string[] = ["John", "Eda", "Maria"];
+
+function show_magicians(magicians: string[]) {
+    magicians.forEach(magician => {
+        console.log(magician);
+    });
+}
+
+show_magicians(magicians);
+// ----------------------------------------------------------------------------------------------------------------------
+function make_great(magicians: string[]) {
+    for (let i = 0; i < magicians.length; i++) {
+        magicians[i] = magicians[i] + " The Love Ones CSS";
+    }
+}
+
+make_great(magicians); // Modifies the original array
+show_magicians(magicians); // Shows modified names
+// ----------------------------------------------------------------------------------------------------------------------
